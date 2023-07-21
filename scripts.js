d7b402ee3a7759f0c62c9dd4bb3abf9ad5b2e4fb
toMiniStars <109888179+toMiniStars@users.noreply.github.com>
2023-07-21 16:44:06 +0100

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,17 +1,13 @@
-// Sample data (sorted in ascending order by date)
 const data = [
   { date: "1947-01-01", gdp: 243.1 },
   { date: "1948-01-01", gdp: 269.4 },
   { date: "1949-01-01", gdp: 267.3 },
-  // Add more data points here...
 ];
 
-// Set up the dimensions and margins for the chart
 const margin = { top: 30, right: 30, bottom: 50, left: 60 };
 const width = 800 - margin.left - margin.right;
 const height = 400 - margin.top - margin.bottom;
 
-// Create the SVG container
 const svg = d3.select("#chart")
   .append("svg")
   .attr("width", width + margin.left + margin.right)
@@ -19,7 +15,6 @@ const svg = d3.select("#chart")
   .append("g")
   .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
-// Create the x-axis and y-axis scales
 const xScale = d3.scaleBand()
   .domain(data.map(d => d.date))
   .range([0, width])
@@ -29,11 +24,9 @@ const yScale = d3.scaleLinear()
   .domain([0, d3.max(data, d => d.gdp)])
   .range([height, 0]);
 
-// Create the x-axis and y-axis using D3 axis function
 const xAxis = d3.axisBottom(xScale);
 const yAxis = d3.axisLeft(yScale);
 
-// Append the x-axis and y-axis to the chart
 svg.append("g")
   .attr("id", "x-axis")
   .attr("transform", `translate(0, ${height})`)
@@ -43,7 +36,6 @@ svg.append("g")
   .attr("id", "y-axis")
   .call(yAxis);
 
-// Create the bars for each data point
 svg.selectAll(".bar")
   .data(data)
   .enter()
@@ -57,7 +49,6 @@ svg.selectAll(".bar")
   .attr("data-date", d => d.date)
   .attr("data-gdp", d => d.gdp);
 
-// Add a title to the chart
 svg.append("text")
   .attr("id", "title")
   .attr("x", width / 2)
